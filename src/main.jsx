c753256b431faea45f18268a0b6f80819575f7e1
Reid Singleton <reidwarnersingleton@gmail.com>
2024-09-05 14:02:48 -0400

    fixed path for App.css

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './app.css'
+import './App.css'
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
 if (process.env.NODE_ENV === 'production') disableReactDevTools();
