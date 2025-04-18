5587a46af9ff156d647054ce45035059dbe166b8
Reid Singleton <reidwarnersingleton@gmail.com>
2025-04-17 21:16:03 -0400

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0',
-    port: 5173,
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === 'production';
+
+  return {
+    plugins: [react()],
+    base: isProduction ? '/irs/' : '/',
+    server: {
+      host: '0.0.0.0',
+      port: 5173,
+      proxy: {
+        '/api': {  // Assuming your API calls are prefixed with "/api"
+          target: 'http://localhost:5000',  // Container's address (use the container's IP or localhost)
+          changeOrigin: true,  // Needed to change the origin of the request
+          rewrite: (path) => path.replace(/^\/api/, ''), // Optional: Adjust path if needed
+        },
+      },
+    },
   }
-})
+});
